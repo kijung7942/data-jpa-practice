bb2e16b91247ca569757edecab14ff00e91fddf9
kijung7942@naver.com <kijung7942@naver.com>
2023-03-24 18:10:40 +0900

    ## 요약 > Repository 연관관계 테스트
    # 기타
    1. Member 엔티티 작성
    2. Team 엔티티 작성
    3. test code 작성하여 Member와 team간의 연관관계 설정 잘 되었는지 확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,29 +1,43 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
 @Setter
+@ToString(of = {"id", "username", "age"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        changeTeam(team);
     }
 
-    public Member(String username) {
-        this.username = username;
+    public void changeTeam(Team team) {
+        if (Objects.nonNull(this.team)) {
+            this.team.setMembers(this.team.getMembers().stream().filter(m -> !m.getId().equals(this.id)).collect(Collectors.toList()));
+        }
+        if (team != null) {
+            this.team = team;
+            team.getMembers().add(this);
+        }
     }
 
 }
